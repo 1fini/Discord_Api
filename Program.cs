4ed62e132e44e05b1451323ed7184616f9c21923
Dan <hinomura.yo@gmail.com>
2022-07-19 11:53:11 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord.WebSocket;
+using Discord;
+
+namespace CES
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient();
+            client.Ready += () => 
+            {
+                Console.WriteLine("Je suis prêt");
+                return Task.CompletedTask;
+            };
+
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("AccessToken",EnvironmentVariableTarget.Process));
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+    }
+}
+
