7ef8306efabadaaa827443023f98a07bb2f9335e
Dan <hinomura.yo@gmail.com>
2022-07-19 14:33:15 +0000

    init 2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,74 @@
 ﻿using Discord.WebSocket;
+using Discord.Commands;
 using Discord;
+using Microsoft.Extensions.DependencyInjection;
+namespace CES;
 
-namespace CES
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
+    => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+    private DiscordSocketClient client;
+    private DiscordSocketRestClient client2;
+    private CommandService command;
+
+    public async Task RunBotAsync()
     {
-        static void Main(string[] args)
-        => new Program().RunBotAsync().GetAwaiter().GetResult();
-
-        private DiscordSocketClient client;
-
-        public async Task RunBotAsync()
+        
+        // You should dispose a service provider created using ASP.NET
+        // when you are finished using it, at the end of your app's lifetime.
+        // If you use another dependency injection framework, you should inspect
+        // its documentation for the best way to do this.
+        using (var services = ConfigureServices())
         {
-            client = new DiscordSocketClient();
-            client.Ready += () => 
-            {
-                Console.WriteLine("Je suis prêt");
-                return Task.CompletedTask;
-            };
+            var client = services.GetRequiredService<DiscordSocketClient>();
 
-            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("AccessToken",EnvironmentVariableTarget.Process));
+            client.Log += LogAsync;
+            services.GetRequiredService<CommandService>().Log += LogAsync;
+
+            // Tokens should be considered secret data and never hard-coded.
+            // We can read from the environment variable to avoid hard coding.
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("AccessToken"));
             await client.StartAsync();
 
-            await Task.Delay(-1);
+            // Here we initialize the logic required to register our commands.
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+            await Task.Delay(Timeout.Infinite);
         }
     }
+
+    private async Task HandleCommandAsync(SocketMessage pMessage)
+    {
+        var message = (SocketUserMessage)pMessage;
+        if(message == null) return;
+        //Console.WriteLine($"Message Reçu: {message}");
+
+        //int argPos = 0;
+
+        //var context = new SocketCommandContext(client, message);
+        //var result = await command.ExecuteAsync(context, argPos, null);
+
+        //if (!result.IsSuccess)
+        //{
+            //await context.Channel.SendMessageAsync(result.ErrorReason);
+        //}
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log.ToString());
+
+        return Task.CompletedTask;
+    }
+
+    private ServiceProvider ConfigureServices() 
+        => new ServiceCollection()
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton<CommandService>()
+            .AddSingleton<CommandHandlingService>()
+    //        .AddSingleton<HttpClient>()
+            .BuildServiceProvider();
 }
 
